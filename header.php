35114e333371a37458bc81e1331127373292805f
Eduardo Meira <eduardo0ws.m@gmail.com>
2024-01-17 10:42:33 -0300

    Revert "Enviar projeto pro repositório"
    This reverts commit 46f66adef96185dbc33a8832db5b95cc86a367cb.

--- header.php
+++
@@ -1,7 +0,0 @@
-<header>
-<div class = "header">
-        <img src = "src/Pics/Logow.png" id = "logo">
-        <h1 class = "title">HeatWiki</h1>
-        <img src = "src/Pics/Logow.png" id = "logo">
-</div>
-</header>
